b5c9baa8b3d8b3bf6404b47e8be9f10e2e839b60
5v3n <sven.kraeuter@web.de>
2010-07-11 13:18:58 +0200

    added page title generator

--- lib/blog_helper.rb
+++ lib/blog_helper.rb
@@ -1,21 +1,33 @@
 # Some useful feature for serious, toto and the likes. Basically, any ruby based blog or site.
 module BlogHelper
-  # create a list of links to tagged articles, default link_format: <a href=/tagged?tag=&+tag+%& >#{tag}</a>
+  # create a list of links to tagged articles, default link_format: %&<a href="/tagged?tag=#{tag}" alt="articles concerning #{tag}" >#{tag}</a> &
   def BlogHelper.create_tag_link_list(csv_string)
     # read csv-string into array
     tag_list = process_to_array(csv_string)
     if tag_list
       tag_string = ""
+      #TODO pass a format via parameter
       tag_list.each { |tag| tag_string << %&<a href="/tagged?tag=#{tag}" alt="articles concerning #{tag}" >#{tag}</a> & }
     end
     tag_string
   end
   # processes a csv-string into an array
   def BlogHelper.process_to_array(csv_string)
-    if csv_string
       #split & handle forgotten spaces after the separator. then flatten the multidemnsional array:
-      result = csv_string.split(', ').map{ |e| e.split(',')}.flatten
-    end
+      csv_string.split(', ').map{ |e| e.split(',')}.flatten if csv_string
+  end
+  # pass the path (@path for a toto blog) & the desired SEO ending, e.g. the name of your blog.
+  # example for toto: <code>generate_title_from_path(@path, title, "mysite.com") will produce 'subpage | mysite.com' as seo friendly page title.</code>
+  def BlogHelper.generate_title(path, title, seo_ending)
+    #TODO use custom title separator...
+       if path == 'index'
+        page_title = seo_ending
+       elsif path.split('/').compact.length == 4
+        page_title = title << " | #{seo_ending}"
+       else
+        page_title = path.capitalize.gsub(/[-]/, ' ') << " | #{seo_ending}"
+      end
+      page_title
   end
 =begin
 Generates javascript to include to the bottom of your index page.
